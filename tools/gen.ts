5dff7e965fd6c1a0cd3a930c5d9989bf8289d9f2
johncoffee <github2017@ragelse.dk>
2018-09-16 18:15:36 +0200

    write test

--- tools/gen.ts
+++ tools/gen.ts
@@ -1,15 +1,16 @@
-import { writeFile } from 'fs-extra'
+import { remove, writeFile } from 'fs-extra'
 import { join } from 'path'
 
-const contractName = process.argv[2]
-const solContractName = contractName.replace(/^[\d_]+/, '')
+const command = process.argv[2]
+const inputName = process.argv[3]
+const normalizedName = inputName.replace(/^[\d_]+/, '')
 
-console.assert(contractName, "Please provide a name, eg. 01_HelloWorld")
+console.assert(inputName, "Please provide a name, eg. 01_HelloWorld")
 
 const solContractTpl = `
 pragma solidity ^0.4.24;
 
-contract ${solContractName} {
+contract ${normalizedName} {
     
     constructor() public
     {
@@ -18,18 +19,45 @@ contract ${solContractName} {
 `.trim()
 
 const migrationJsTpl = `
-const c = artifacts.require("./${solContractName}.sol")
+const c = artifacts.require("./${normalizedName}.sol")
 
-module.exports = function(deployer) {
+function deployContract(deployer) { 
   deployer.deploy(c)
 }
+
+module.exports = deployContract
 `.trim()
 
-const testJsTpl = ``
+const testJsTpl = `
+const ${normalizedName} = artifacts.require("${normalizedName}")
+
+contract('${normalizedName}', function(accounts) {
+
+  it("should ", async () => {
+    const instance = await ${normalizedName}.deployed()
+    // const a = await instance.fieldName.call()
+    // assert.equal(a, b, "")
+  })
+
+})
+`.trim()
+
 const testSolTpl = ``
 
-const solPath = join(__dirname, "../", 'truffle/contracts', contractName + '.sol')
-const migPath = join(__dirname, "../", 'truffle/migrations', contractName + '.js')
+const migPath =   join(__dirname, "../", 'truffle/migrations', inputName + '.js')
+const solPath =   join(__dirname, "../", 'truffle/contracts', inputName + '.sol')
+const jsTestPath= join(__dirname, "../", 'truffle/test', inputName + '.test.js')
+const solTestPath=join(__dirname, "../", 'truffle/test', inputName + '.test.sol')
 
-writeFile(migPath, migrationJsTpl)
-writeFile(solPath, solContractTpl)
+if (command === "create" || command === "mk") {
+  writeFile(migPath, migrationJsTpl)
+  writeFile(solPath, solContractTpl)
+  writeFile(jsTestPath, testJsTpl)
+}
+else if (command === "rm") {
+  remove(migPath)
+  remove(solPath)
+  remove(jsTestPath)
+  remove(solTestPath)
+}
+
