69deee930173900979e19daa06e5fb189a13fa19
johncoffee <github2017@ragelse.dk>
2018-09-24 21:32:39 +0200

    migration in TypeScript

--- tools/gen.js
+++ tools/gen.js
@@ -1,51 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_extra_1 = require("fs-extra");
-const path_1 = require("path");
-const command = process.argv[2];
-const inputName = process.argv[3];
+var fs_extra_1 = require("fs-extra");
+var path_1 = require("path");
+var command = process.argv[2];
+var inputName = process.argv[3];
 console.assert((command === "create" || command === "mk" || command === "rm"), "Please provide a command, eg. `create 01_HelloWorld`");
 console.assert(inputName, "Please provide a contract name, eg. 01_HelloWorld");
-const normalizedName = inputName.replace(/^[\d_]+/, '');
-const solContractTpl = `
-pragma solidity ^0.4.24;
-
-contract ${normalizedName} {
-    
-    constructor() public
-    {
-    }   
-}
-`.trim();
-const migrationJsTpl = `
-const c = artifacts.require("./${normalizedName}.sol")
-
-function deployContract(deployer) { 
-  deployer.deploy(c)
-}
-
-module.exports = deployContract
-`.trim();
-const testTsTpl = `
-const ${normalizedName} = artifacts.require("${normalizedName}")
-
-contract('${normalizedName}', function(accounts) {
-
-  it("should ", async () => {
-    const instance = await ${normalizedName}.deployed()
-    // const a = await instance.fieldName.call()
-    // assert.equal(a, b, "")
-  })
-
-})
-`.trim();
-const testSolTpl = ``;
-const migPath = path_1.join(__dirname, "../", 'truffle/migrations', inputName + '.js');
-const solPath = path_1.join(__dirname, "../", 'truffle/contracts', inputName + '.sol');
-const tsTestPath = path_1.join(__dirname, "../", 'truffle/test', inputName + '.test.ts');
-const solTestPath = path_1.join(__dirname, "../", 'truffle/test', inputName + '.test.sol');
+var normalizedName = inputName.replace(/^[\d_]+/, '');
+var solContractTpl = ("\npragma solidity ^0.4.24;\n\ncontract " + normalizedName + " {\n    \n    constructor() public\n    {\n    }   \n}\n").trim();
+var migrationTpl = ("\nimport Deployer = Truffle.Deployer\nimport Migration = Truffle.Migration\nimport Accounts = Truffle.Accounts\n\nconst c = artifacts.require(\"./" + normalizedName + ".sol\")\n\nfunction deployContract(deployer:Deployer, network: string, accounts: Accounts) {\n  deployer.deploy(c)\n}\n\nmodule.exports = (deployContract as Migration)\n").trim();
+var testTsTpl = ("\nconst " + normalizedName + " = artifacts.require(\"" + normalizedName + "\")\n\ncontract('" + normalizedName + "', function(accounts) {\n\n  it(\"should \", async () => {\n    const instance = await " + normalizedName + ".deployed()\n    // const a = await instance.fieldName.call()\n    // assert.equal(a, b, \"\")\n  })\n\n})\n").trim();
+var testSolTpl = "";
+var migPath = path_1.join(__dirname, "../", 'truffle/migrations', inputName + '.ts');
+var solPath = path_1.join(__dirname, "../", 'truffle/contracts', inputName + '.sol');
+var tsTestPath = path_1.join(__dirname, "../", 'truffle/test', inputName + '.test.ts');
+var solTestPath = path_1.join(__dirname, "../", 'truffle/test', inputName + '.test.sol');
 if (command === "create" || command === "mk") {
-    fs_extra_1.writeFile(migPath, migrationJsTpl);
+    fs_extra_1.writeFile(migPath, migrationTpl);
     fs_extra_1.writeFile(solPath, solContractTpl);
     fs_extra_1.writeFile(tsTestPath, testTsTpl);
 }
