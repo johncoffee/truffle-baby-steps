9829af32a53c896d2cc5f6250d190f4d1ce4a7e6
johncoffee <github2017@ragelse.dk>
2019-04-10 20:57:06 +0200

    remove StateMachine

--- ethereum/migrations/21_StateMachine.js
+++
@@ -1,7 +0,0 @@
-const c = artifacts.require("./StateMachine.sol")
-
-function deployContract(deployer, network, accounts) {
-  deployer.deploy(c)
-}
-
-module.exports = deployContract
--- ethereum/migrations/22_StateMachineIssue.js
+++
@@ -1,7 +0,0 @@
-const c = artifacts.require("./StateMachineIssue.sol")
-
-function deployContract(deployer) { 
-  deployer.deploy(c)
-}
-
-module.exports = deployContract
